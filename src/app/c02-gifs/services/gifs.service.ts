eab0b792c2a2c6e572327782d1f3416bccad23c4
Sergio Cortés <sergio.cp@hotmail.es>
2022-08-17 19:52:51 +0000

    asincrono

--- src/app/c02-gifs/services/gifs.service.ts
+++ src/app/c02-gifs/services/gifs.service.ts
@@ -6,20 +6,32 @@ import { Gifs } from 'src/app/gif.interface';
 })
 export class GifsService {
 
+    // Url del Sitio Web
+    private servicioUrl = "https://api.giphy.com/v1/gifs/"
+  
+    // ApiKey Obtenida del Sitio
+    private apiKey = "ajUjHÑJKLDJKLSÑ[SÑDDJDLDJKDLfUCD9v"
+    
+    // Arreglo de Historia
+    private _historia:String[]=["ultraman","heman","messi","chavo","jerry"]
+    
+    //Resultados obtenidos del Backend
+    private resultados:Gifs[]=[]
+
+
     // Solo para visualizar cuando se ejecuta por primera vez
     constructor() { console.log("Constructor GifsService   *******************  ")}
 
 
-  // Método que ejecutará el componente buscar y el sidebar
-  buscarGifts(query:string){
-    console.log("buscarGifts final debiera ser Observable")
+// Método que ejecutará el componente buscar y el sidebar
+buscarGifts(query:string){
+  console.log("buscarGifts final debiera ser Observable")
 
-    // Revisa el historial
-    query = this.revisaHistoria(query)
-    
-    // Ejecuta el método del back-end como sincrónico
-    this.buscarGiftsSincronico(query)
-  }
+  // Revisa el historial
+  query = this.revisaHistoria(query)
+  //this.buscarGiftsSincronico(query)
+  this.buscarGiftsAsincronico(query)
+}
 
     // Retorna el arreglo de _Historial
     get getHistorial(){
@@ -35,19 +47,7 @@ export class GifsService {
       return [...this.resultados]
     }
 
-    // Url del Sitio Web
-    private servicioUrl = "https://api.giphy.com/v1/gifs/"
-  
-    // ApiKey Obtenida del Sitio
-    private apiKey = "ajUjHÑJKLDJKLSÑ[SÑDDJDLDJKDLfUCD9v"
-    
-    // Arreglo de Historia
-    private _historia:String[]=["ultraman","heman","messi","chavo","jerry"]
-    
-    //Resultados obtenidos del Backend
-    private resultados:Gifs[]=[]
-
-      // Revisa si la palabra esta en el arreglo
+  // Revisa si la palabra esta en el arreglo
   // Si se encuentra no hace nada
   revisaHistoria(query:string):string{ 
     //Transforma a minusculas 
@@ -75,9 +75,9 @@ export class GifsService {
     if (query.trim().length==0) return
 
 //********************************************************** */
-   //let resp = await fetch("https://api.giphy.com/v1/gifs/search$1api_key=XXXXX&q=messi&limit=10")
+   //let resp = await fetch("https://api.giphy.com/v1/gifs/search?api_key=XXXXX&q=messi&limit=10")
    // configuramos ls url, con la api_key, la palabra y el limite
-   let stUrl = `${this.servicioUrl}search$2api_key=${this.apiKey}&q=${query}&limit=10`
+   let stUrl = `${this.servicioUrl}search?api_key=${this.apiKey}&q=${query}&limit=10`
    
    // Para poder ejecutarlo localmente, 
    // siempre y cuando haya iniciado el servicio JSonServer
@@ -103,5 +103,40 @@ export class GifsService {
 
    // Observe que sin hacer nada se redespliega el resultado
   }
+
+   // Asincrónico
+   buscarGiftsAsincronico(query:String){
+    //let resp = await fetch("https://api.giphy.com/v1/gifs/search?api_key=XXXXX&q=messi&limit=10")
+    // configuramos ls url, con la api_key, la palabra y el limite
+    let stUrl = `${this.servicioUrl}search?api_key=${this.apiKey}&q=${query}&limit=10`
+ 
+    // Para poder ejecutarlo localmente, 
+    // siempre y cuando haya iniciado el servicio JSonServer
+    // Si se inscribio en https://api.giphy.com
+    // elimine la siguiente línea
+    stUrl = "http://localhost:3000/" + query
+ 
+    // Revisamos en la consola la url
+    console.log(stUrl)
+ 
+ //********************************************************** */
+    // Observe la secuencia de los log
+    console.log("1111111111111111111111111111111111111111")
+    fetch(stUrl)
+        .then(resp=>{
+             console.log("22222222222222222222222222222222222")
+             resp.json().then(data=>{
+               console.log("AASincronizador ",data)
+                  // Copia la respuesta al arreglo resultados
+                this.resultados = data.data
+                console.log("3333333333333333333333333333333")
+ 
+             })
+             console.log("4444444444444444444444444444444444444444") 
+        })
+    console.log("555555555555555555555555555555555555555555555")     
+   }
+
+   
 
 }
